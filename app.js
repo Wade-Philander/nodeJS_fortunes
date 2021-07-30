3a46c6adac741203c269eee93add9d26aa8a3867
Wade-Philander <wadephilander7@gmail.com>
2021-07-30 14:25:06 +0200

    updated

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 // Displaying fortunes in web using a res & req.
 
+const fs = require('fs');
 const express = require('express');
-
-
+const bodyParser = require('body-parser');
 const app = express();
-
 const fortunes = require('./data/fortune');
+
+app.use(bodyParser.json());
 
 app.get('/fortunes',(req, res) => {
     res.json(fortunes)
 });
 
+
 // Random function
 // fetching a random fortune from data fortune 
 // Localhost:3000/fortunes/random
+
+
 
 app.get('/fortunes/random',(req, res) => {
 
@@ -30,6 +34,28 @@ app.get('/fortunes/:id', (req, res) => {
     console.log(req.params);
 
     res.json(fortunes.find(f => f.id == req.params.id));
+});
+
+// making/adding a new fortune.
+
+app.post('/fortunes', (req, res) => {
+    console.log(req.body);
+
+    const {message, lucky_number, spiral_animal } = req.body;
+
+    const fortune_ids = fortunes.map(f => f.id);
+
+    const fortune = { 
+        id: (fortune_ids.length > 0 ? Math.max(...fortune_ids) : 0) + 1, 
+        message, 
+        lucky_number, 
+        spiral_animal
+    };
+    const new_fortunes = fortunes.concat(fortune);
+
+    fs.writeFile('./data/fortunes.json', JSON.stringify(new_fortunes), err => console.log(err));//adds the new fortune created with the curl into the json file we have inside data 
+
+    res.json(new_fortunes)
 });
 
 module.exports = app;
